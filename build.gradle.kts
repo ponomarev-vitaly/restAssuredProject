0eb6201a529318435a22a9f45bf99c652f3341c1
Vitaly Ponomarev <vp211082@gmail.com>
2023-05-23 12:02:16 +0300

    I modified the code.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(
+            "io.rest-assured:rest-assured:5.3.0"
+    )
 }
 
 tasks.test {
