2e51dcc8957e066c86eabc909758c9f370e73b0c
Vitaly Ponomarev <vp211082@gmail.com>
2023-05-23 12:11:18 +0300

    I used .log() method to get log specification.

--- src/main/java/RestAssuredRunner.java
+++ src/main/java/RestAssuredRunner.java
@@ -3,6 +3,7 @@ import io.restassured.RestAssured;
 public class RestAssuredRunner {
     public static void main(String[] args) { // Create main method.
         RestAssured.given() // Create object.
+                .log().all()
                 .baseUri("https://api.trello.com")
                 .get();
 
