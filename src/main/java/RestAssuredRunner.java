e188476d16dd35c1204ae5924dbadd0d5a1c1578
Vitaly Ponomarev <vp211082@gmail.com>
2023-05-23 12:06:05 +0300

    I created main method and object.

--- src/main/java/RestAssuredRunner.java
+++ src/main/java/RestAssuredRunner.java
@@ -1,5 +1,10 @@
+import io.restassured.RestAssured;
+
 public class RestAssuredRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // Create main method.
+        RestAssured.given() // Create object.
+                .baseUri("https://api.trello.com")
+                .get();
 
     }
 }
