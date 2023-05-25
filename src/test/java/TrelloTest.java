ba3125eac75a1b530099846ecdb2a3979e184b58
Vitaly Ponomarev <vp211082@gmail.com>
2023-05-26 00:09:47 +0300

    I fixed the issues and ran the test successfully.

--- src/test/java/TrelloTest.java
+++ src/test/java/TrelloTest.java
@@ -1,8 +1,9 @@
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class TrelloTest {
-
+    @BeforeAll
     public static void setBaseUrl(){
         RestAssured.baseURI = "https://api.trello.com";
     }
