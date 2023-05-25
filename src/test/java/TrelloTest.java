18c440b91997a4a82b7727adeda5318668d5c711
Vitaly Ponomarev <vp211082@gmail.com>
2023-05-25 23:42:02 +0300

    I modified the code, but still have to fix the code.

--- src/test/java/TrelloTest.java
+++ src/test/java/TrelloTest.java
@@ -2,11 +2,14 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 
 public class TrelloTest {
+
+    public static void setBaseUrl(){
+        RestAssured.baseURI = "https://api.trello.com";
+    }
     @Test
     public void checkTrelloApi() { // Create a method.
         RestAssured.given() // Create object.
                 .log().all()
-                .baseUri("https://api.trello.com")
                 .get()
                 .prettyPeek()
                 .then()
