7493ebebf2898c20263ae58ad4a9a54f8284fc7a
Vitaly Ponomarev <vp211082@gmail.com>
2023-05-26 21:27:49 +0300

    I modified the code.

--- src/test/java/GetCardsTest.java
+++ src/test/java/GetCardsTest.java
@@ -13,8 +13,8 @@ public class GetCardsTest {
     @Test
     public void checkGetCards(){
         obj.requestWithAuth()
-                .pathParam("list", "646746aecb24dbfdcd185387")
-                .get("/1/lists/{list}/cards")
+                .pathParam("list_id", "646746aecb24dbfdcd185387")
+                .get("/1/lists/{list_id}/cards")
                 .then()
                 .statusCode(200);
     }
@@ -22,8 +22,8 @@ public class GetCardsTest {
     @Test
     public void checkGetCard(){
         obj.requestWithAuth()
-                .pathParam("id", "646748eaef222a0de8dfb52c")
-                .get("/1/cards/{id}")
+                .pathParam("card_id", "646748eaef222a0de8dfb52c")
+                .get("/1/cards/{card_id}")
                 .then()
                 .statusCode(200);
     }
