ffaeb32b99f6297ae7f093470788299033ac18d2
Vitaly Ponomarev <vp211082@gmail.com>
2023-05-26 19:23:37 +0300

    I added new Test method to get a specific board.

--- src/test/java/GetBoardsTest.java
+++ src/test/java/GetBoardsTest.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -20,5 +21,19 @@ public class GetBoardsTest {
                 .get("/1/members/{member}/boards")
                 .then()
                 .statusCode(200);
+    }
+
+    @Test
+    public void checkGetBoard(){
+        RestAssured.given() // Create object.
+                .queryParams(Map.of(
+                        "key", "36a241085b4ad3d2267dbd22fc544c5d",
+                        "token", "ATTAefa2c1bf12159579a4153dc7d77b4ca9055612b58ba8c673c1b0a04d6ad834c3EB4CCA0D"
+                ))
+                .pathParam("id", "646746aecb24dbfdcd185380")
+                .get("/1/boards/{id}")
+                .then()
+                .statusCode(200)
+                .body("name", Matchers.equalTo("New Board"));
     }
 }
