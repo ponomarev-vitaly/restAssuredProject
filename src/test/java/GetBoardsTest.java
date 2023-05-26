7134e3d12f92c948a62c0adf8e112e67c24f601a
Vitaly Ponomarev <vp211082@gmail.com>
2023-05-26 19:46:01 +0300

    I created new method requestWithAuth() and put the .given(), .queryParams() methods in it. Run the test successfully.

--- src/test/java/GetBoardsTest.java
+++ src/test/java/GetBoardsTest.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -12,28 +13,29 @@ public class GetBoardsTest {
     }
     @Test
     public void checkGetBoards() { // Create a method.
-        RestAssured.given() // Create object.
-                .queryParams(Map.of(
-                        "key", "36a241085b4ad3d2267dbd22fc544c5d",
-                        "token", "ATTAefa2c1bf12159579a4153dc7d77b4ca9055612b58ba8c673c1b0a04d6ad834c3EB4CCA0D"
-                ))
+        requestWithAuth()
                 .pathParam("member", "vitalyponomarev3")
                 .get("/1/members/{member}/boards")
                 .then()
                 .statusCode(200);
     }
 
+
     @Test
     public void checkGetBoard(){
-        RestAssured.given() // Create object.
-                .queryParams(Map.of(
-                        "key", "36a241085b4ad3d2267dbd22fc544c5d",
-                        "token", "ATTAefa2c1bf12159579a4153dc7d77b4ca9055612b58ba8c673c1b0a04d6ad834c3EB4CCA0D"
-                ))
+        requestWithAuth()
                 .pathParam("id", "646746aecb24dbfdcd185380")
                 .get("/1/boards/{id}")
                 .then()
                 .statusCode(200)
                 .body("name", Matchers.equalTo("New Board"));
+    }
+
+    private static RequestSpecification requestWithAuth() {
+        return RestAssured.given() // Create object.
+                .queryParams(Map.of(
+                        "key", "36a241085b4ad3d2267dbd22fc544c5d",
+                        "token", "ATTAefa2c1bf12159579a4153dc7d77b4ca9055612b58ba8c673c1b0a04d6ad834c3EB4CCA0D"
+                ));
     }
 }
