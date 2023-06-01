076aa7f84daa2bfc595bef0cd95e1c2274ae03eb
Vitaly Ponomarev <vp211082@gmail.com>
2023-06-02 01:21:06 +0300

    I added three methods checkGetCardWIthInvalidId(), checkGetCardWIthInvalidAuth(), checkGetCardWithAnotherUserCredentials() and requestWithAuth(). I ran the test, all worked well.

--- src/test/java/GetCardsValidationTest.java
+++ src/test/java/GetCardsValidationTest.java
@@ -1,8 +1,11 @@
 import io.restassured.RestAssured;
-import io.restassured.module.jsv.JsonSchemaValidator;
-import org.hamcrest.Matchers;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 public class GetCardsValidationTest {
     @BeforeAll
@@ -13,46 +16,48 @@ public class GetCardsValidationTest {
     GetBoardsTest obj;
 
     @Test
-    public void checkGetCards(){
-        obj.requestWithAuth()
-                .pathParam("list_id", "646746aecb24dbfdcd185387")
-                .get("/1/lists/{list_id}/cards")
+    public void checkGetCardWIthInvalidId(){ // This is the test to check if the user sends invalid id.
+        Response response = requestWithAuth()
+                .pathParam("card_id", "invalid")
+                .get("/1/cards/{card_id}");
+        response
                 .then()
-                .statusCode(200);
+                .statusCode(400);
+        Assertions.assertEquals("invalid id", response.body().asString());
     }
 
     @Test
-    public void checkGetCard(){
-        obj.requestWithAuth()
+    public void checkGetCardWIthInvalidAuth() { // This is the test for the situation when the user tries to access a board without access key and token.
+        Response response = RestAssured.given()
                 .pathParam("card_id", "646748eaef222a0de8dfb52c")
-                .get("/1/cards/{card_id}")
+                .get("/1/cards/{card_id}");
+        response
                 .then()
-                .statusCode(200)
-                .body("name", Matchers.equalTo("#test card name updated"));
+                .statusCode(401);
+        Assertions.assertEquals("unauthorized card permission requested", response.body().asString());
     }
 
     @Test
-    public void checkGetCardsValidation() { // Create a method, which is for the JSON schema validation only.
-        obj.requestWithAuth()
-                .queryParams("fields", "id,name")
-                .pathParam("list_id", "646746aecb24dbfdcd185387")
-                .get("/1/lists/{list_id}/cards")
-                .then()
-                .statusCode(200)
-                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/get_cards.json"));
-    }
-
-
-    @Test
-    public void checkGetCardValidation(){ // Create a method, which is for the JSON schema validation only.
-        obj.requestWithAuth()
-                .queryParams("fields", "id,name")
+    public void checkGetCardWithAnotherUserCredentials(){
+        // This is the test for the situation when the user uses the key and token from the another user.
+        Response response = RestAssured.given()
+                .queryParams(Map.of(
+                        "key", "8b32218e6887516d17c84253faf967b6",
+                        "token", "492343b8106e7df3ebb7f01e219cbf32827c852a5f9e2b8f9ca296b1cc604955"
+                ))
                 .pathParam("card_id", "646748eaef222a0de8dfb52c")
-                .get("/1/cards/{card_id}")
+                .get("/1/cards/{card_id}");
+        response
                 .then()
-                .statusCode(200)
-                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/get_card.json"))
-//                .body("name", Matchers.equalTo("New Board")); // This is the line of code with the incorrect card name.
-                .body("name", Matchers.equalTo("#test card name updated")); // This is the line of code with the correct card name.
+                .statusCode(401);
+        Assertions.assertEquals("invalid token", response.body().asString());
+    }
+
+    public static RequestSpecification requestWithAuth() {
+        return RestAssured.given() // Create object.
+                .queryParams(Map.of(
+                        "key", "36a241085b4ad3d2267dbd22fc544c5d",
+                        "token", "ATTAefa2c1bf12159579a4153dc7d77b4ca9055612b58ba8c673c1b0a04d6ad834c3EB4CCA0D"
+                ));
     }
 }
