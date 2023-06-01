c34460af2e7c3d457ca9449161c73f3124b0e249
Vitaly Ponomarev <vp211082@gmail.com>
2023-06-01 12:47:06 +0300

    I created 2 new tests for the JSON schema validation in the GetCardsTest java class. I also added 2 JSON schemas: get_cards.json and get_card.json with the actual JSON schemas. I run the tests, all test passed.

--- src/test/java/GetCardsTest.java
+++ src/test/java/GetCardsTest.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -28,5 +29,30 @@ public class GetCardsTest {
                 .then()
                 .statusCode(200)
                 .body("name", Matchers.equalTo("#test card name updated"));
+    }
+
+    @Test
+    public void checkGetCardsValidation() { // Create a method, which is for the JSON schema validation only.
+        obj.requestWithAuth()
+                .queryParams("fields", "id,name")
+                .pathParam("list_id", "646746aecb24dbfdcd185387")
+                .get("/1/lists/{list_id}/cards")
+                .then()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/get_cards.json"));
+    }
+
+
+    @Test
+    public void checkGetCardValidation(){ // Create a method, which is for the JSON schema validation only.
+        obj.requestWithAuth()
+                .queryParams("fields", "id,name")
+                .pathParam("card_id", "646748eaef222a0de8dfb52c")
+                .get("/1/cards/{card_id}")
+                .then()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/get_card.json"))
+//                .body("name", Matchers.equalTo("New Board")); // This is the line of code with the incorrect card name.
+                .body("name", Matchers.equalTo("#test card name updated")); // This is the line of code with the correct card name.
     }
 }
