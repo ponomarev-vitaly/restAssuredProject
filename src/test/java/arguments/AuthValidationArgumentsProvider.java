67e0e0009951a9148415617a0cdd98333d22ef43
Vitaly Ponomarev <vp211082@gmail.com>
2023-06-03 22:40:41 +0300

    I modified the code and had the problem while running the test, try to fix it.

--- src/test/java/arguments/AuthValidationArgumentsProvider.java
+++ src/test/java/arguments/AuthValidationArgumentsProvider.java
@@ -17,10 +17,14 @@ public class AuthValidationArgumentsProvider implements ArgumentsProvider {
                         Collections.emptyMap()
                 ),
                 new AuthValidationArgumentsHolder(
-                        Map.of("key", "36a241085b4ad3d2267dbd22fc544c5d")
+                        Map.of(
+                                "key", "36a241085b4ad3d2267dbd22fc544c5d"
+                        )
                 ),
                 new AuthValidationArgumentsHolder(
-                        Map.of("token", "ATTAefa2c1bf12159579a4153dc7d77b4ca9055612b58ba8c673c1b0a04d6ad834c3EB4CCA0D")
+                        Map.of(
+                                "token", "ATTAefa2c1bf12159579a4153dc7d77b4ca9055612b58ba8c673c1b0a04d6ad834c3EB4CCA0D"
+                        )
                 )
         ).map(Arguments::of);
     }
