f28acaeefa68b07566a0b66f7e2e3bf2b7c49971
Vitaly Ponomarev <vp211082@gmail.com>
2023-06-09 00:30:55 +0300

    I created new class CreateBoardValidationTest java class.

--- src/test/java/arguments/providers/BoardNameValidationArgumentsProvider.java
+++ src/test/java/arguments/providers/BoardNameValidationArgumentsProvider.java
@@ -1,8 +1,10 @@
 package arguments.providers;
 
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Stream;
 
@@ -11,7 +13,8 @@ public class BoardNameValidationArgumentsProvider implements ArgumentsProvider {
     @Override
     public Stream provideArguments(ExtensionContext context){
         return Stream.of(
-                Map.of("name", 1234)
-        );
+                Map.of("name", 1234),
+                Collections.emptyMap()
+        ).map(Arguments::of);
     }
 }
---
+++ src/test/java/test/create/CreateBoardValidationTest.java
@@ -0,0 +1,15 @@
+package test.create;
+
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.util.Map;
+
+public class CreateBoardValidationTest {
+    @ParameterizedTest
+
+    public void checkCreateBoardWithInvalidName(Map bodyParams){
+
+
+
+    }
+}
