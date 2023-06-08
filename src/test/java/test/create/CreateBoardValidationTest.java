fd0d42db7f7c082182e22906661808c2b617ed50
Vitaly Ponomarev <vp211082@gmail.com>
2023-06-09 01:12:49 +0300

    I modified the code and run the test successfully.

--- src/test/java/test/create/CreateBoardValidationTest.java
+++ src/test/java/test/create/CreateBoardValidationTest.java
@@ -1,15 +1,28 @@
 package test.create;
 
+import arguments.providers.BoardNameValidationArgumentsProvider;
+import consts.BoardsEndpoints;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import test.BaseTest;
 
 import java.util.Map;
 
-public class CreateBoardValidationTest {
+public class CreateBoardValidationTest extends BaseTest {
     @ParameterizedTest
-
+    @ArgumentsSource(BoardNameValidationArgumentsProvider.class)
     public void checkCreateBoardWithInvalidName(Map bodyParams){
-
-
-
+        Response response = requestWithAuth()
+                .body(bodyParams)
+                .contentType(ContentType.JSON)
+                .post(BoardsEndpoints.CREATE_BOARD_URL);
+        response
+                .then()
+                .statusCode(400);
+        String errorMessage = response.jsonPath().getString("message");
+        Assertions.assertEquals("invalid value for name", errorMessage);
     }
 }
