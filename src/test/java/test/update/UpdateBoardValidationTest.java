261c9b2d1633e66afc1d0c2c5659c9c90584779e
Vitaly Ponomarev <vp211082@gmail.com>
2023-06-11 09:15:34 +0300

    I added method checkGetBoardWithAnotherUserCredentials() in the test class. I run the test successfully.

--- src/test/java/test/update/UpdateBoardValidationTest.java
+++ src/test/java/test/update/UpdateBoardValidationTest.java
@@ -9,6 +9,7 @@ import consts.UrlParamValues;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 import test.BaseTest;
@@ -43,4 +44,20 @@ public class UpdateBoardValidationTest extends BaseTest {
                 .statusCode(401);
         Assertions.assertEquals(validationArguments.getErrorMessage(), response.body().asString());
     }
+
+    @Test
+    public void checkGetBoardWithAnotherUserCredentials(){
+        // This is the test for the situation when the user uses the key and token from the another user.
+        Response response = requestWithoutAuth()
+                .queryParams(UrlParamValues.ANOTHER_USER_AUTH_QUERY_PARAMS)
+                .pathParam("id", UrlParamValues.EXISTING_BOARD_ID)
+                .body(Map.of("name", "UpdatedName"))
+                .contentType(ContentType.JSON)
+                .put(BoardsEndpoints.UPDATE_BOARD_URL);
+        response
+                .then()
+                .statusCode(401);
+        Assertions.assertEquals("invalid token", response.body().asString());
+    }
 }
+
