ceffb376bbe74cb82bed3c73b056100b19853112
Vitaly Ponomarev <vp211082@gmail.com>
2023-06-11 10:30:31 +0300

    I added 2 methods createBoard() and checkDeleteBoard(). I have to fix several issues in the code and run the DeleteBoardTest again.

--- src/test/java/test/delete/DeleteBoardTest.java
+++ src/test/java/test/delete/DeleteBoardTest.java
@@ -1,19 +1,39 @@
 package test.delete;
 
 import consts.BoardsEndpoints;
+import consts.UrlParamValues;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import test.BaseTest;
 
 import java.util.Map;
 
 public class DeleteBoardTest extends BaseTest {
-    private String createBoardId;
+    private String createdBoardId;
+    @BeforeEach
     public void createBoard(){
-        createBoardId = requestWithAuth()
-                .body(Map.of("name", "New Board"))
+        createdBoardId = requestWithAuth()
+                .body(Map.of("name", "#1New Board"))
                 .contentType(ContentType.JSON)
                 .put(BoardsEndpoints.CREATE_BOARD_URL)
                 .body().jsonPath().get("id");
+    }
 
+    @Test
+    public void checkDeleteBoard(){
+        requestWithAuth()
+                .pathParam("id", createdBoardId)
+                .delete(BoardsEndpoints.DELETE_BOARD_URL)
+                .then()
+                .statusCode(200)
+                .body("_value", Matchers.equalTo(null));
+        requestWithAuth()
+                .queryParam("fields", "id,name")
+                .pathParam("member", UrlParamValues.USER_NAME)
+                .get(BoardsEndpoints.GET_ALL_BOARDS_URL)
+                .then()
+                .body("id", Matchers.not(Matchers.hasItem(createdBoardId)));
     }
 }
