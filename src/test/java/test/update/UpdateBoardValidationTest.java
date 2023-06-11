19a3d1be007f80660f063b1fe41d4f5a88bf7789
Vitaly Ponomarev <vp211082@gmail.com>
2023-06-11 09:06:30 +0300

    I added method checkUpdateBoardWithInvalidId() in the test class. I run the test successfully.

--- src/test/java/test/update/UpdateBoardValidationTest.java
+++ src/test/java/test/update/UpdateBoardValidationTest.java
@@ -2,6 +2,9 @@ package test.update;
 
 import arguments.holders.BoardIdValidationArgumentsHolder;
 import arguments.providers.BoardIdValidationArgumentsProvider;
+import consts.BoardsEndpoints;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 import test.BaseTest;
@@ -11,6 +14,12 @@ public class UpdateBoardValidationTest extends BaseTest {
     @ArgumentsSource(BoardIdValidationArgumentsProvider.class)
 
     public void checkUpdateBoardWithInvalidId(BoardIdValidationArgumentsHolder argumentsHolder){
-
+        Response response = requestWithAuth()
+                .pathParams(argumentsHolder.getPathParams())
+                .put(BoardsEndpoints.UPDATE_BOARD_URL);
+        response
+                .then()
+                .statusCode(argumentsHolder.getStatusCode());
+        Assertions.assertEquals(argumentsHolder.getErrorMessage(), response.body().asString());
     }
 }
